af6e18b67dba350c98c32352a45618eb6123c503
LucasDemartini <Lucas.demartini@institutoivoti.com.br>
2017-11-07 19:39:16 -0200

    Pontuação

--- Futebol.cs
+++ Futebol.cs
@@ -10,7 +10,13 @@ namespace Futebol
             System.Console.WriteLine("Menu");
             System.Console.WriteLine("Enter - Adiciona time e pontuação \n0 - FInaliza e exibe a tabela");
 
-            While (Console.ReadKey().Key == ConsoleKey.Enter) 
+            While (Console.ReadKey().Key == ConsoleKey.Enter) {
+                System.Console.WriteLine("Diga o nome do time: ");
+                string time = Console.ReadLine();
+
+                 System.Console.WriteLine("Diga a pontuação do time: ");
+                 int pont = convert.ToInt32(Console.ReadLine());
+            }
 
         }
     }
