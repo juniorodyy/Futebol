2573ddc8b1cf22f94c93f556ca44a23fb083ef9f
LucasDemartini <junior.ody2000@gmail.com>
2017-11-28 21:11:01 -0200

    Program

--- Program.cs
+++ Program.cs
@@ -9,7 +9,33 @@ namespace Brasileirao
             ConsoleKeyInfo a;
             TabelaBrasileirao t = new TabelaBrasileirao();
 
-           
+           using System;
+
+namespace Brasileirao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo a;
+            TabelaBrasileirao t = new TabelaBrasileirao();
+
+            do {
+                System.Console.WriteLine("Menu:");
+                System.Console.WriteLine("1 - Informe novo time e sua pontuação \n2 - Finaliza e exibe a tabela");
+                System.Console.WriteLine("Informe sua opção:");
+                a = Console.ReadKey();
+                if (a.Key == ConsoleKey.D1) {
+                    System.Console.WriteLine("Digite o nome do time:");
+                    string time = Console.ReadLine();
+                    System.Console.WriteLine("Digite a pontuação:");
+                    string pont = Console.ReadLine();
+                    t.times.Add(new Times(time, Int32.Parse(pont)));
+                }
+            } while (a.Key != ConsoleKey.D2);
+
+            System.Console.WriteLine("Tabela"); 
+            t.resultado();
         }
     }
 }
